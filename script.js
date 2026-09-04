6446a90ef0d63283b90473961cda92562eee4f7f
modyosama849-prog <modyosama849@gmail.com>
2026-09-05 02:14:47 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function changeText() {
+    document.getElementById("jobText").innerHTML =
+        "Coding is fun! I want to create amazing games and websites!";
+}
+
+function changeText() {
+    document.getElementById("jobText").innerHTML =
+        "Coding is fun! I want to create amazing games and websites!";
+
+    document.body.style.backgroundColor = "lightyellow";
+}
